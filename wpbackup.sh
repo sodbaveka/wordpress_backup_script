a7d9fe47b070c3f541fabfc075b3f3ed8c2648b6
sodbaveka <sodbaveka@gmail.com>
2021-05-05 22:31:27 +0200

    refactoring

--- wpbackup.sh
+++ wpbackup.sh
@@ -17,17 +17,13 @@ db_name="WordPress"
 db_username="theseus"
 db_password="theseus"
 
-# log
-log_file_name="/var/log/wpbackuplog.txt"
-sudo touch $log_file_name;
-sudo chmod 766 $log_file_name;
 echo "Script exécuté le" $NOW
 
 # backup MYSQL database, gzip it and send to backup folder.
-mysqldump --opt -u$db_username  -p$db_password $db_name | gzip > $backup_folder_path/$db_backup_name 2>> $log_file_name
+mysqldump --opt -u$db_username  -p$db_password $db_name | gzip > $backup_folder_path/$db_backup_name && echo "dump mysql OK"
 
 # create a tarball of the wordpress files, gzip it and send to backup folder.
-tar -czf $backup_folder_path/$wpfiles_backup_name --transform $WWW_TRANSFORM $wp_folder 2>> $log_file_name
+tar -czvf $backup_folder_path/$wpfiles_backup_name --transform $WWW_TRANSFORM $wp_folder && echo "tar archive OK"
 
 # delete all but 2 recent wordpress database back-ups (files having .sql.gz extension) in backup folder.
 find $backup_folder_path -maxdepth 1 -name "*.sql.gz" -type f | xargs -x ls -t | awk 'NR>2' | xargs -L1 rm
@@ -41,15 +37,11 @@ sftp wpsftp@srv-sftp-01 << EOF
 cd wpbackup
 pwd
 put *.*
-exit
 EOF
 
 # rotation of backups to avoid an accumulation of backup files
-ssh -t theseus@srv-sftp-01 "
-cd /home/wpsftp/wpbackup
-find -maxdepth 1 -name '*.sql.gz' -type f | xargs -x ls -t | awk 'NR>2' | xargs -L1 rm
-find -maxdepth 1 -name '*.tar.gz' -type f | xargs -x ls -t | awk 'NR>2' | xargs -L1 rm
-"
+ssh theseus@srv-sftp-01 "cd /home/wpsftp/wpbackup
+find /home/wpsftp/wpbackup -maxdepth 1 -name '*.sql.gz' -type f | xargs -x ls -t | awk 'NR>2' | xargs -L1 rm 2>> /dev/null
+find /home/wpsftp/wpbackup -maxdepth 1 -name '*.tar.gz' -type f | xargs -x ls -t | awk 'NR>2' | xargs -L1 rm 2>> /dev/null"
 
-# Task Scheduling: WordPress Backups with Crontab
-# TO DO
+
