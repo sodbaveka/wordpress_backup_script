4e92afe471787e536300b75796adca46b3f37b4e
sodbaveka <sodbaveka@gmail.com>
2021-05-07 10:54:46 +0200

    refactoring

--- wprestore.sh
+++ wprestore.sh
@@ -17,19 +17,19 @@ mysql_password="theseus"
 mkdir $destination_folder_path 2> /dev/null
 
 # Retrieving the names of the files to copy
-ssh -t $user_login@$ftp_server '
+ssh  -i ~/.ssh/id_ecdsa $user_login@$ftp_server '
 cd /home/wpsftp/wpbackup
 find -maxdepth 1 -name "*.sql.gz" -type f | xargs -x ls -tr | awk "NR>1" | xargs -L1 basename > .temp_file.tmp
 find -maxdepth 1 -name "*.tar.gz" -type f | xargs -x ls -tr | awk "NR>1" | xargs -L1 basename >> .temp_file.tmp
 '
 
 cd $destination_folder_path
-scp $user_login@$ftp_server:"$source_folder_path"/.temp_file.tmp .temp_file.tmp && echo "list of files to download copied"
+scp  -i ~/.ssh/id_ecdsa $user_login@$ftp_server:"$source_folder_path"/.temp_file.tmp .temp_file.tmp && echo "list of files to download copied"
 
 # Downloading files from ftp server with secure connection
 while read ligne
 do 		
-	sftp $user_login@$ftp_server:"$source_folder_path"/"$ligne"
+	sftp  -i ~/.ssh/id_ecdsa $user_login@$ftp_server:"$source_folder_path"/"$ligne"
 done < .temp_file.tmp && echo "All downloads executed"
 
 # # Restoring wordpress database
@@ -39,7 +39,7 @@ read db_backup_name < <(find -maxdepth 1 -name "*.sql" -type f | xargs -L1 basen
 mysql --user="$mysql_user" --password="$mysql_password" "$mysql_db" < "$db_backup_name" && echo "database restored"
 rm $db_backup_name
 
-# Restoring wordpress files
+# Restoring wordpress files & deleting backup files
 read files_backup_name < <(cat .temp_file.tmp | sed -n '2 p')
 tar -xzf $files_backup_name && echo "$files_backup_name décompressé"
 rm $files_backup_name
