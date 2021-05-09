00eca07b02dca2405db0a8e7bd3a255bef2dde84
sodbaveka <sodbaveka@gmail.com>
2021-05-09 07:44:12 +0200

    rsync test

--- wpbackup.sh
+++ wpbackup.sh
@@ -3,7 +3,7 @@
 # Debug mode
 #set -x 
 
-# my backups will use these filenames.
+# Variables.
 NOW=$(date +"%Y-%m-%d-%H%M")
 db_backup_name="wp-db-backup-$NOW.sql.gz"
 wpfiles_backup_name="wp-files-backup-$NOW.tar.gz"
@@ -12,20 +12,21 @@ user_login="theseus"
 ftp_server="srv-sftp-01"
 log_file="/home/$user_login/wpbackup.log"
 
-# path to my backup folder.
+# Path to my backup folder.
 backup_folder_path="/home/theseus/wp_backup"
+inc_backup_folder_dest="/home/theseus/inc_wp_backup"
 
-# path to my WordPress website
+# Path to my WordPress website
 wp_folder="/var/www/sodbaveka/wordpress"
 
-# database connection info.
+# Database connection info.
 db_name="WordPress"
 db_username="theseus"
 db_password="theseus"
 
 # Log file
 touch "$log_file" && echo "log file created"
-echo "#######################################################################################################################" >> $log_file 2>&1
+echo "#######################################################################################################################" > $log_file 2>&1
 echo "Début d'exécution du script le" $NOW >> $log_file 2>&1
 echo "***********************************************************************************************************************" >> $log_file 2>&1
 
@@ -33,19 +34,19 @@ echo "Début d'exécution du script le" $NOW
 
 mkdir "$backup_folder_path" >> $log_file 2>&1
 
-# backup MYSQL database, gzip it and send to backup folder.
+# Backup MYSQL database, gzip it and send to backup folder.
 mysqldump --opt -u$db_username  -p$db_password $db_name | gzip > $backup_folder_path/$db_backup_name && echo "dump mysql OK" 
 
-# create a tarball of the wordpress files, gzip it and send to backup folder.
+# Create a tarball of the wordpress files, gzip it and send to backup folder.
 tar -czf $backup_folder_path/$wpfiles_backup_name --transform $WWW_TRANSFORM $wp_folder >> $log_file 2>&1 && echo "tar archive OK"
 
-# delete all but 2 recent wordpress database back-ups (files having .sql.gz extension) in backup folder.
+# Delete all but 2 recent wordpress database back-ups (files having .sql.gz extension) in backup folder.
 find $backup_folder_path -maxdepth 1 -name "*.sql.gz" -type f | xargs -x ls -t | awk 'NR>2' | xargs -L1 rm >> $log_file 2>&1
 
-# delete all but 2 recent wordpress files back-ups (files having .tar.gz extension) in backup folder.
+# Delete all but 2 recent wordpress files back-ups (files having .tar.gz extension) in backup folder.
 find $backup_folder_path -maxdepth 1 -name "*.tar.gz" -type f | xargs -x ls -t | awk 'NR>2' | xargs -L1 rm >> $log_file 2>&1
 
-# Copy to ftp server with secure connection
+# Full copy to ftp server with secure connection
 cd /home/theseus/wp_backup
 sftp  -i /home/"$user_login"/.ssh/id_rsa $user_login@$ftp_server << EOF
 cd /home/wpsftp/wpbackup
@@ -53,7 +54,13 @@ pwd
 put *.*
 EOF
 
-# rotation of backups to avoid an accumulation of backup files
+# An incremental backup of wordpress files...just for fun ;-)
+rsync -az --stats -e ssh -i /home/"$user_login"/.ssh/id_rsa $wp_folder $user_login@$ftp_server:$inc_backup_folder_dest >> $log_file 2>&1
+status=$?
+echo '***'
+echo "rsync return (0 if success; other if error) : " $status  >> $log_file 2>&1
+
+# Rotation of backups to avoid an accumulation of backup files
 ssh -i /home/"$user_login"/.ssh/id_rsa $user_login@$ftp_server << EOF
 cd /home/wpsftp/wpbackup
 find /home/wpsftp/wpbackup -maxdepth 1 -name '*.sql.gz' -type f | xargs -x ls -t | awk 'NR>2' | xargs -L1 rm 2>> /dev/null
