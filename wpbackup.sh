1e45e8db0b9a45caa0b55cc2c399e4613ee615fd
sodbaveka <sodbaveka@gmail.com>
2021-05-12 07:31:34 +0200

    comments update

--- wpbackup.sh
+++ wpbackup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Bash script to automate the backup of a wordpress website
+
+# Copyright: (c) 2021, Mickaël Duchet <sodbaveka@gmail.com>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+
 # Debug mode
 #set -x 
 
